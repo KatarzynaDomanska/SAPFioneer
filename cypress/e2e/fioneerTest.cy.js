8efd1cc7cacae1a27396a7f45fb48b1d6409ba82
katarzynadomanska <katarzyna.domanska2@gmail.com>
2023-11-04 14:30:02 +0100

    first test

--- cypress/e2e/fioneerTest.cy.js
+++ cypress/e2e/fioneerTest.cy.js
@@ -4,20 +4,25 @@ describe('Fioneer QA Automation Challenge', () => {
     beforeEach(() => {
         cy.visit('https://www.sapfioneer.com/');
         cy.clearCookies()
-    })
+    });
 
     it('Check if header bookmarks are visible', () => {
-        const expectedBookmarks = ["Banking", "Insurance", "Finance & ESG", "Services", "Partners", "Company", "Resources"]
+        const expectedBookmarks = ['Banking', 'Insurance', 'Finance & ESG', 'Services', 'Partners', 'Company', 'Resources'];
 
-        cy.get('.hide-for-medium.flex-right > .header-nav')
-            .find('a')
-            .invoke('text')
-            .then((bookmarks) => {
-                expect(bookmarks.lenght).to.equal(expectedBookmarks.length);
+        cy.get('.hide-for-medium.flex-right > .header-nav > li > a')
+            .filter(':lt(7)')
+            .should('be.visible')
+            .each((bookmark, a) => {
+                cy.wrap(bookmark).invoke('text').should('equal', expectedBookmarks[a])
+            });
+    });
 
-                for (let i = 0; i < expectedBookmarks.length; i++) {
-                    expect(bookmarks[i]).to.equal(expectedBookmarks[i]);
-                }
-            })
+    it('Check if user has been redirected to the correct page', () => {
+        const expectedPage = 'https://www.sapfioneer.com/finance-esg/financial-control/';
+
+        cy.get('#menu-item-29979 > .nav-top-link').click()
+        //cy.get('#col-1514298497 > .col-inner > .ux-menu > :nth-child(4) > .ux-menu-link__link').click()
+
     })
 })
+
